a0a8e7ae2c18d94ff14ed4ba8980f164dc759e57
Ryuichi Watanabe <ryucrosskey@gmail.com>
2019-07-30 00:57:15 +0900

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -3,6 +3,40 @@ import logging
 import sys
 
 
+class Logger:
+    def __init__(self, name=__name__):
+        self.logger = getLogger(name)
+        self.logger.setLevel(DEBUG)
+        formatter = Formatter("[%(asctime)s] [%(process)d] [%(name)s] [%(levelname)s] %(message)s")
+
+        handler = StreamHandler()
+        handler.setLevel(DEBUG)
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+        handler = handlers.RotatingFileHandler(filename = 'your_log_path.log',
+                                               maxBytes = 1048576,
+                                               backupCount = 3)
+        handler.setLevel(DEBUG)
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warn(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+
 class NoPassFilter(logging.Filter):
     """
     Create a log-only class and make it inherit
