c5a192f2fa3d4234c5bf76ea52d0c16845878e01
Ryuichi Watanabe <ryucrosskey@gmail.com>
2019-09-09 23:09:39 +0900

    fix package info

--- setup.py
+++ setup.py
@@ -4,3 +4,14 @@
 from setuptools import setup
 import os
 from codecs import open
+
+with open("README.rst", "r") as f:
+    readme = f.read()
+
+
+def get_packages(package):
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
