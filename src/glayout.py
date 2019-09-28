8d86466297a7699a71436ecd9f8f0b436313212c
Ryuichi Watanabe <ryucrosskey@gmail.com>
2019-09-28 13:18:08 +0900

    Update glayout.py

--- src/glayout.py
+++ src/glayout.py
@@ -7,3 +7,19 @@ cv2.imwrite("./sample_glay.jpg", gray)
 from imgcat import imgcat
 imgcat(open("./sample_glay.jpg"))
 
+if __name__ == "__main__":
+      PARSER = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=RawTextHelpFormatter)
+    PARSER.add_argument("--ip", "-i", required=True,
+                        help="OME Appliance IP")
+    PARSER.add_argument("--user", "-u", required=True,
+                        help="Username for OME Appliance",
+                        default="admin")
+    PARSER.add_argument("--password", "-p", required=True,
+                        help="Password for OME Appliance")
+    PARSER.add_argument("--reportid", "-r", required=True,
+                        help="a valid report id to execute")
+    PARSER.add_argument("--groupid", "-g", required=False,
+                        default=0,
+                        help="Optional param - Group id to run report against")
+    ARGS = PARSER.parse_args()
